9adf6eb12191d1652c647da68670de30a9059762
Izak Burger <isburger@gmail.com>
2012-02-11 23:35:57 +0200

    implement friendly-name storage component

--- src/slc/mailrouter/interfaces.py
+++ src/slc/mailrouter/interfaces.py
@@ -5,3 +5,19 @@ from slc.mailrouter import MessageFactory as _
 class IMailRouterLayer(Interface):
     """Marker Interface used by as BrowserLayer
     """
+
+class IFriendlyNameStorage(Interface):
+    """ A place to store the mapping of friendly names to uids, allowing
+        a user to email friendlyname@ourdomain instead of uid@ourdomain. """
+
+    def add(uid, name):
+        """ Map name -> uid. """
+
+    def remove(uid):
+        """ Remove mapping. """
+
+    def get(name):
+        """ Look up name, return the uid. """
+
+    def __len__():
+        """ Return the number of items in storage. """
---
+++ src/slc/mailrouter/storage.py
@@ -0,0 +1,37 @@
+from persistent import Persistent
+from zope.interface import implements
+from BTrees.OOBTree import OOBTree, OOSet
+from slc.mailrouter.interfaces import IFriendlyNameStorage
+
+class FriendlyNameStorage(Persistent):
+    implements(IFriendlyNameStorage)
+
+    def __init__(self):
+        self._forward = OOBTree() # name -> uid
+        self._reverse = OOBTree() # uid -> name
+
+    def add(self, uid, name):
+        """ Map name -> uid. """
+        self._forward[name] = uid
+        self._reverse[uid] = name
+
+    def remove(self, uid):
+        """ Remove mapping. This will be called when a folder is deleted,
+            therefore we use the uid. """
+        marker = object()
+        name = self._reverse.get(uid, marker)
+        if name is not marker:
+            del(self._reverse[uid])
+            try:
+                del(self._forward[name])
+            except KeyError:
+                # If it isn't there, good, that is the outcome we wanted,
+                # right?
+                pass
+
+    def get(self, name, _marker=None):
+        """ Look up name, map uid to an object and return it. """
+        return self._forward.get(name, _marker)
+
+    def __len__(self):
+        return len(self._forward)
