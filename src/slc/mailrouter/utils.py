21b3c0154179c9c0d2d7b243d7a927292a89ac74
Manuel Reinhardt <reinhardt@syslab.com>
2012-07-18 13:00:23 +0100

    include sender and context (if applicable) in error messages

--- src/slc/mailrouter/utils.py
+++ src/slc/mailrouter/utils.py
@@ -68,7 +68,7 @@ class MailToFolderRouter(object):
             #user_id = pm.searchMembers('email', sender)[0]['username']
             user = pm.getMemberById(sender).getUser()
         except (IndexError, AttributeError):
-            raise NotFoundError(_("Sender is not a valid user"))
+            raise NotFoundError(_("%s is not a valid user address" % sender))
         #user = pm.getMemberById(user_id).getUser()
 
         self.acl_users = getToolByName(site, 'acl_users')
@@ -76,7 +76,7 @@ class MailToFolderRouter(object):
 
         # Check permissions
         if not getSecurityManager().checkPermission('Add portal content', context):
-            raise PermissionError(_("Insufficient privileges"))
+            raise PermissionError(_("%s has insufficient privileges on %s" % (sender, context.getId())))
 
         # Defer actual work to an adapter
         result = IMailImportAdapter(context).add(msg)
@@ -136,7 +136,7 @@ class MailToGroupRouter(object):
             #user_id = pm.searchMembers('email', sender)[0]['username']
             user = pm.getMemberById(sender).getUser()
         except IndexError:
-            raise NotFoundError(_("Sender is not a valid user"))
+            raise NotFoundError(_("%s is not a valid user address" % sender))
             
         # get members and send messages
         members = group.getGroupMembers()
