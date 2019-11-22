1d984c2c1d7a7ffc091394ac8ea0240ccb12c2f3
Manuel Reinhardt <reinhardt@syslab.com>
2019-11-22 10:52:31 +0100

    Dropped support for Plone 4.

--- setup.py
+++ setup.py
@@ -8,7 +8,15 @@ setup(
     description="Framework for handling email in zope",
     long_description=open("README.md").read(),
     # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=["Programming Language :: Python", "Framework :: Plone",],
+    classifiers=[
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
+    ],
     keywords="smtp2zope email mailin zope",
     author="Syslab.com GmbH",
     author_email="info@syslab.com",
@@ -20,8 +28,8 @@ setup(
     include_package_data=True,
     package_data={"slc.mailrouter": ["sql/*"]},
     zip_safe=False,
-    extras_require={"test": ["mock", "plone.app.testing",],},
-    install_requires=["setuptools", "plone.api", "Products.CMFPlone",],
+    extras_require={"test": ["mock", "plone.app.testing"]},
+    install_requires=["setuptools", "plone.api", "Products.CMFPlone>=5.0"],
     entry_points="""
         [z3c.autoinclude.plugin]
         target = plone
